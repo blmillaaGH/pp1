87614df25bc10c1d82459c6139d8592ee08d7e9a
LautaroAiello <lautaroaiello34@gmail.com>
2024-09-02 14:53:46 -0300

    intento de hacer los modals del menu, falta finalizar

--- script.js
+++ script.js
@@ -31,26 +31,131 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //Inicio de sesion
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+// const login = document.getElementById("loginForm");
+// login.addEventListener("submit", function(event) {
+//   event.preventDefault();
   
-  const nombre = document.getElementById("username").value.trim();
-  const contraseña = document.getElementById("password").value.trim();
-  const mensajeError = document.getElementById("error-message");
+//   const nombre = document.getElementById("username").value.trim();
+//   const contraseña = document.getElementById("password").value.trim();
+//   const mensajeError = document.getElementById("error-message");
 
   
-  if (nombre === "" || contraseña === "") {
-      mensajeError.textContent = "Por favor, completa todos los campos.";
-      return;
-  }
+//   if (nombre === "" || contraseña === "") {
+//       mensajeError.textContent = "Por favor, completa todos los campos.";
+//       return;
+//   }
 
-  const usuarioValido = "123";
-  const contraseñaValida = "123";
+//   const usuarioValido = "123";
+//   const contraseñaValida = "123";
 
-  if (nombre === usuarioValido && contraseña === contraseñaValida) {
-      alert("Bienvenido a tu cuenta!");
-      window.location.href = "menus.html";
-  } else {
-      mensajeError.textContent = "Usuario o contraseña incorrectos.";
-  }
+//   if (nombre === usuarioValido && contraseña === contraseñaValida) {
+//       alert("Bienvenido a tu cuenta!");
+//       window.location.href = "menus.html";
+//   } else {
+//       mensajeError.textContent = "Usuario o contraseña incorrectos.";
+//   }
+// });
+
+//Modals
+
+const modales = document.querySelectorAll('.miModal');
+const botones = document.querySelectorAll('.abrirModal');
+const cerrar = document.querySelectorAll('.cerrarModal');
+
+console.log(modales);
+
+botones.forEach((boton, index) => {
+    boton.onclick = function () {
+        modales[index].style.display = 'block';
+        mostrarContenido(modales[index]);
+    }
 });
+
+
+cerrar.forEach((cerrartBtn, index) => {
+    cerrartBtn.onclick = function () {
+        modales[index].style.display = 'none'; 
+    }
+})
+
+
+
+window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('miModal')) {
+        event.target.style.display = 'none';
+    }
+});
+
+function mostrarContenido (modal)  {
+    const contenidoModal = modal.querySelector('.modalContenido');
+    contenidoModal.innerHTML = `<table class="tablaPedidos">
+                            <thead>
+                                <tr>
+                                    <th>MENU DEL DÍA</th>
+                                    <th>ACCIÓN</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>(menú ejemplo del día disponible)</td>
+                                    <td>
+                                        <button>AGREGAR</button>
+                                        <button class="eliminar">ELIMINAR</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    
+                        <div style="margin-top: 20px; text-align: right;">
+                            <button style="background-color: #28a745;">Confirmar pedido del día</button>
+                        </div>`; 
+}
