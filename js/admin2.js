7db8cf0a91552d52ff55490c2c86c0f7e27df1c5
Sebastian <j4guarsj4@gmail.com>
2024-11-03 00:57:28 -0300

    desde menu, ahora se traen las comidas disponibles, falta poder enviar los datos (dia, semana y comida id)

---
+++ js/admin2.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    cargarComidas();
+});
+
+function cargarComidas() {
+    fetch("http://localhost:8080/api/comidas")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('NETWORK ERROR' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data); // chequear qué llega en los log.
+            const selects = document.querySelectorAll('.comidaSelector');
+            selects.forEach(select => {
+                select.innerHTML = '<option value="" disabled selected>Seleccionar comida</option>';
+                data.forEach(comida => {
+                    const option = document.createElement('option');
+                    option.value = comida.id;
+                    option.textContent = comida.nombre;
+                    select.appendChild(option);
+                });
+            });
+        })
+        .catch(error => console.error('Error al cargar las comidas:', error));
+}
